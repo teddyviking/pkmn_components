79b472378449f9d542d4881dcef64fdb40b417fb
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-09-28 18:42:18 -0400

    Factor out URI method in Pokemon API model

--- app/models/pokemon_api.rb
+++ app/models/pokemon_api.rb
@@ -1,9 +1,13 @@
 class PokemonApi
   POKEMON_API_URL = "http://pokeapi.co/api/v1"
 
-  def self.request(path)
+  def self.uri(path)
     url_string = "#{POKEMON_API_URL}/#{path}/"
-    uri = URI.parse(url_string)
+    return URI.parse(url_string)
+  end
+
+  def self.request(path)
+    uri = self.uri(path)
     response = Net::HTTP.get_response(uri)
     return response.body
   end
